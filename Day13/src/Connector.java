52f693d76294bab8fafc3dbc3d2811644187b9ab
bitcampa <bitcampa@bitcamp>
2019-05-13 10:09:38 +0900

    ssss

---
+++ Day13/src/Connector.java
@@ -0,0 +1,31 @@
+import java.io.InputStream;
+import java.net.URL;
+
+public class Connector {
+	
+	public static void main(String[] args) throws Exception{
+		
+		for(int i = 0; i< 300; i++) {
+			
+			new Thread(
+					
+					()->{
+						try {
+							URL url = new URL("http://192.168.41.43:8080/web1/hello.jsp");
+							
+							InputStream in = url.openStream();
+							byte[] arr = new byte[8192];
+							in.read(arr);
+							System.out.println(new String(arr));
+						}catch(Exception e) {
+					}
+					
+					}
+					
+					).start();
+			
+			
+		}
+	}
+
+}
